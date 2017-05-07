6754b349df82af3781e8281be73f21d13de4775f
Christoph Blechschmitt <cblechschmitt@hiorg-server.de>
2017-05-07 23:21:21 +0200

    Send comment to GitLab before publishing to SonarQube (or we won't find any new issues)

--- sonar-scanner-run.sh
+++ sonar-scanner-run.sh
@@ -5,7 +5,11 @@
   if [ ! -z "$SONAR_SOURCES" ]; then
     COMMAND="$COMMAND -Dsonar.sources=\"$SONAR_SOURCES\""
   else
-    SONAR_SOURCES=$(pwd)
+    if [ ! -z "$CI_PROJECT_DIR" ]; then
+      SONAR_SOURCES=$CI_PROJECT_DIR
+    else
+      SONAR_SOURCES=$(pwd)
+    fi
     if [ ! -z "$SONAR_SCAN_SUBDIR" ]; then
       SONAR_SOURCES="$SONAR_SOURCES/$SONAR_SCAN_SUBDIR"
     fi
@@ -46,26 +50,25 @@
     COMMAND="$COMMAND -X"
   fi
 
-
   if $SONAR_GITLAB_COMMENT ; then
+    CMD2="$COMMAND -Dsonar.analysis.mode=\"issues\""
+    if [ ! -z "$CI_PROJECT_ID" ]; then
+      CMD2="$CMD2 -Dsonar.gitlab.project_id=\"$CI_PROJECT_ID\""
+    fi
+    if [ ! -z "$CI_COMMIT_SHA" ]; then
+      CMD2="$CMD2 -Dsonar.gitlab.commit_sha=\"$CI_COMMIT_SHA\""
+    fi
+    if [ ! -z "$CI_COMMIT_REF_NAME" ]; then
+      CMD2="$CMD2 -Dsonar.gitlab.ref_name=\"$CI_COMMIT_REF_NAME\""
+    fi
+    
+    echo "(comment 2 gitlab)"
+    eval $CMD2
+    
     if $SONAR_PUBLISH ; then
       echo "(publish 2 sonar)"
       eval $COMMAND
     fi
-    
-    COMMAND="$COMMAND -Dsonar.analysis.mode=\"issues\""
-    if [ ! -z "$CI_PROJECT_ID" ]; then
-      COMMAND="$COMMAND -Dsonar.gitlab.project_id=\"$CI_PROJECT_ID\""
-    fi
-    if [ ! -z "$CI_COMMIT_SHA" ]; then
-      COMMAND="$COMMAND -Dsonar.gitlab.commit_sha=\"$CI_COMMIT_SHA\""
-    fi
-    if [ ! -z "$CI_COMMIT_REF_NAME" ]; then
-      COMMAND="$COMMAND -Dsonar.gitlab.ref_name=\"$CI_COMMIT_REF_NAME\""
-    fi
-    
-    echo "(comment 2 gitlab)"
-    eval $COMMAND
     
   else
     echo "(publish 2 sonar)"
